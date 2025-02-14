225ca667d6c15d1de412cfaff06fac36a3d09528
fabiomagajr <fabiomagajr@gmail.com>
2025-02-14 15:00:57 -0300

    add buttons

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from PySide6.QtWidgets import QApplication, QPushButton, QWidget,QVBoxLayout
+from PySide6.QtWidgets import QApplication, QPushButton, QWidget,QVBoxLayout,QGridLayout
 import sys
 
 app=QApplication(sys.argv)
-botao=QPushButton("TExto do botao")
-botao.setStyleSheet('font-size: 40px; color: red;')
+botao=QPushButton("Texto do botao")
+botao.setStyleSheet('color: red;')
 botao2=QPushButton("Esse é o botão 2")
 botao2.setStyleSheet('color: blue')
+botao3=QPushButton("esse é o terceiro")
+botao3.setStyleSheet("color: green")
 central_widget = QWidget()
-layout=QVBoxLayout()
+layout=QGridLayout()
 central_widget.setLayout(layout)
-layout.addWidget(botao2)
-layout.addWidget(botao)
+layout.addWidget(botao2,1,1)
+layout.addWidget(botao,1,2)
+layout.addWidget(botao3,3,1,1,2)
 central_widget.show()
 app.exec()
 
